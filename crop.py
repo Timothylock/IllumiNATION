b74245966516d67e9766f60aa6c631ff3543fd5c
Florence Chan <florence@dhcp-10-103-233-132.wireless.northwestern.private>
2015-11-21 18:13:16 -0600

    small changes

--- crop.py
+++ crop.py
@@ -1,4 +1,4 @@
-def crop(filename, x, y, n, m, lengthx, lengthy):
+def crop(filename, n, m, lengthx, lengthy):
 # w and y are the delta-x and delta-y of the images, respectively
 # n and m are the number of lines on the x and y respectively - n x m	
 #note that length x and length are just the overall length of the image
@@ -10,7 +10,7 @@ def crop(filename, x, y, n, m, lengthx, lengthy):
    	for i in range (0,lengthx):
    		for j in range (0,lengthy):
 
-			crop_img = img[stepsizey*j: stepsizey+stepsizey*j, stepsizex*i: stepsizex+x*stepsizex]
+			crop_img = img[stepsizey*j: stepsizey+stepsizey*j, stepsizex*i: stepsizex+stepsizex*i]
 			cv2.imwrite("1.jpg", crop_img)
 			listt[i][j] = str(i) + str(j) + ".jpg"
 
